207095117e7042cafcd10214979fa0473cce51a7
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2019-04-16 19:07:36 -0700

    refactored

--- src/tunecomposer/DeleteCommand.java
+++ src/tunecomposer/DeleteCommand.java
@@ -1,19 +1,30 @@
+/**
+ *
+ * @author aslettcj
+ */
 package tunecomposer;
 
+import java.util.Collection;
 import java.util.Set;
 
 public class DeleteCommand implements Undoable {
-	private static Set<Playable> removedPlayables;
+	private static Collection<Playable> removedPlayables;
 	
-	public void DeleteCommand() {
-		
+	public DeleteCommand(Collection<Playable> toRemove) {
+            removedPlayables = toRemove;
 	}
-	
+
 	public void undo() {
-		
+            for (Playable p : removedPlayables) {
+		p.getRectangle().setVisible(true);
+            }
+            TuneComposer.allPlayables.addAll(removedPlayables);
 	}
 	
 	public void redo() {
-		
+            for (Playable p : removedPlayables) {
+		p.getRectangle().setVisible(false);
+            }
+            TuneComposer.allPlayables.removeAll(removedPlayables);
 	}
 }
