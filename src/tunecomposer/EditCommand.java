4d48a9c7281fba83e225b514db01f124991a5209
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2019-04-18 22:58:10 -0700

    Update EditCommand.java

--- src/tunecomposer/EditCommand.java
+++ src/tunecomposer/EditCommand.java
@@ -5,10 +5,34 @@
  */
 package tunecomposer;
 
+import java.util.Collection;
+
 /**
- *
- * @author taylorkm
+ * Represents the command for editing notes by dragging. Doesn't work.
+ * @author Abbey Felley Colin Aslett Kimberly Taylor Angie Mead
  */
 public class EditCommand {
-    
+    private static Collection<Playable> editedPlayables;
+
+    /**
+     * Constructs the command from the given set of Playables. Probably what
+     * we should do is find a way to tell how far they've moved and do some
+     * math.
+     * @param toEdit 
+     */
+    public EditCommand(Collection<Playable> toEdit) {
+        editedPlayables = toEdit;
+    }
+
+    /**
+     * Un-edits the Playables in the set.
+     */
+    public void undo() {
+    }
+
+    /**
+     * Re-edits the Playables.
+     */
+    public void redo() {
+    }
 }
