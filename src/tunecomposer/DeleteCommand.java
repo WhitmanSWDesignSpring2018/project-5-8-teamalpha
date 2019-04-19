148644b81be63717a13479e0add1cfdaee450953
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2019-04-18 22:57:29 -0700

    Update DeleteCommand.java

--- src/tunecomposer/DeleteCommand.java
+++ src/tunecomposer/DeleteCommand.java
@@ -1,30 +1,40 @@
-/**
- *
- * @author aslettcj
- */
 package tunecomposer;
 
 import java.util.Collection;
-import java.util.Set;
 
+/**
+ * Represents the delete selected notes action.
+ * @author Abbey Felley Colin Aslett Kimberly Taylor Angie Mead
+ */
 public class DeleteCommand implements Undoable {
-	private static Collection<Playable> removedPlayables;
-	
-	public DeleteCommand(Collection<Playable> toRemove) {
-            removedPlayables = toRemove;
-	}
+    private static Collection<Playable> removedPlayables;
 
-	public void undo() {
-            for (Playable p : removedPlayables) {
-		p.getRectangle().setVisible(true);
-            }
-            TuneComposer.allPlayables.addAll(removedPlayables);
-	}
-	
-	public void redo() {
-            for (Playable p : removedPlayables) {
-		p.getRectangle().setVisible(false);
-            }
-            TuneComposer.allPlayables.removeAll(removedPlayables);
-	}
+    /**
+     * Initializes the DeleteCommand.
+     * @param toRemove, the collection of Playables to be deleted
+     */
+    public DeleteCommand(Collection<Playable> toRemove) {
+        removedPlayables = toRemove;
+    }
+
+    /**
+     * Puts back the deleted playables by adding them back to allPlayables.
+     */
+    public void undo() {
+        for (Playable p : removedPlayables) {
+            p.getRectangle().setVisible(true);
+        }
+        TuneComposer.allPlayables.addAll(removedPlayables);
+    }
+
+    /**
+     * Removes the playables from the composition. Removes the playables
+     * from allPlayables and hides the note rectangles from view.
+     */
+    public void redo() {
+        for (Playable p : removedPlayables) {
+            p.getRectangle().setVisible(false);
+        }
+        TuneComposer.allPlayables.removeAll(removedPlayables);
+    }
 }
