1d548673b6d268167d33f2316f0c1fce695620a0
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2019-04-18 23:05:04 -0700

    Update Undoable.java

--- src/tunecomposer/Undoable.java
+++ src/tunecomposer/Undoable.java
@@ -1,4 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tunecomposer;
+
+/**
+ * An interface to represent all the commands that will be undoable.
+ * @author Abbey Felley Colin Aslett Kimberly Taylor Angie Mead
+ */
 
 public interface Undoable {
 	void undo();
