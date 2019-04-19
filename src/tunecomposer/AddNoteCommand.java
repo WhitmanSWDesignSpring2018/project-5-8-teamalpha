401e51153ea2ecbe6e590ab53aacf1565dd407c7
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2019-04-18 22:55:05 -0700

    Update AddNoteCommand.java

--- src/tunecomposer/AddNoteCommand.java
+++ src/tunecomposer/AddNoteCommand.java
@@ -1,25 +1,35 @@
 package tunecomposer;
 
-import javafx.scene.input.MouseEvent;
-
+/**
+ * Represents the action of adding a note 
+ * @author Colin Aslett Abbey Felley Kimberly Taylor Angie Mead
+ */
 public class AddNoteCommand implements Undoable {
-	private static Note note;
-	
-	public AddNoteCommand(Note newNote) {
-		note = newNote;
-	}
-	
-	public void execute() {
-		
-	}
-	
-	public void undo() {
-		TuneComposer.allPlayables.remove(note);
-		note.getRectangle().setVisible(false);
-	}
-	
-	public void redo() {
-		TuneComposer.allPlayables.add(note);
-		note.getRectangle().setVisible(true);
-	}
+    private static Note note;
+
+    /**
+     * Creates the command based on the given note.
+     * @param newNote, the note that the action will be able to do and undo
+     */
+    public AddNoteCommand(Note newNote) {
+            note = newNote;
+    }
+
+    /**
+     * Undoes the addition of the note. Removes the note from allPlayables
+     * and hides the rectangle so that it neither appears nor plays the note
+     */
+    public void undo() {
+            TuneComposer.allPlayables.remove(note);
+            note.getRectangle().setVisible(false);
+    }
+
+    /**
+     * Adds a note to the composition. Adds the stored note to allPlayables 
+     * and shows the rectangle
+     */
+    public void redo() {
+            TuneComposer.allPlayables.add(note);
+            note.getRectangle().setVisible(true);
+    }
 }
