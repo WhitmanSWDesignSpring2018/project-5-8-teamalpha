9eb79a3675f19ef9a7dd6d793901b69e835f84dd
taylorkm <taylorkm@artemis.whitman.edu>
2019-04-30 21:10:16 -0700

    saving xml files

--- src/tunecomposer/XMLParser.java
+++ src/tunecomposer/XMLParser.java
@@ -5,9 +5,15 @@
  */
 package tunecomposer;
 
+import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import jdk.internal.org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  *
@@ -20,9 +26,40 @@ public class XMLParser {
     
         try{
             DocumentBuilder builder = factory.newDocumentBuilder(); 
+            Document document = builder.parse("testfile.txt");  //get file name here
+            NodeList noteList = document.getElementsByTagName("note");
+            NodeList gestureList = document.getElementsByTagName("gesture");
+            for (int i = 0; i<noteList.getLength(); i++){
+                Node n = noteList.item(i);
+                if (n.getNodeType() == Node.ELEMENT_NODE){
+                    Element note = (Element) n; 
+                    String x = note.getAttribute("x"); 
+                    String y = note.getAttribute("y"); 
+                    String instrument = note.getAttribute("instrument"); 
+                    String width = note.getAttribute("width"); 
+                    String isSelected = note.getAttribute("isSelected"); 
+                }
+            }
+            for (int j = 0; j<gestureList.getLength(); j++){
+                Node g = gestureList.item(j);
+                if (g.getNodeType() == Node.ELEMENT_NODE){
+                    Element gesture = (Element) g; 
+                    String isSelected = gesture.getAttribute("isSelected"); 
+                    NodeList gestureChildrenList = g.getChildNodes();
+                    for (int k=0; k<gestureChildrenList.getLength(); k++) {
+                        
+                    }
+                }
+            }
         } catch (ParserConfigurationException e){
         
+        } catch (SAXException e){
+            
+        }catch (IOException e){
+            
         }
+        
+        
     }
     
     
